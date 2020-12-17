e357c3f0996aeff77a39fd2bbf8d4f200a42dd7e
Mykhailo_Pinchuk <kingmetok@gmail.com>
2020-12-17 14:12:19 +0200

    Heroku plugin

--- src/main/webapp/test.js
+++ src/main/webapp/test.js
@@ -13,12 +13,12 @@ form.addEventListener("submit", (e) => {
 
     xhr.open("POST", url, true);
     xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.onreadystatechange = function () {
-        // if (xhr.readyState === 4 && xhr.status === 200) {
-            // const json = JSON.parse(xhr.responseText);
-            // console.log(json.nickname + ", " + json.password);
-        // }
-    };
+    // xhr.onreadystatechange = function () {
+    //     // if (xhr.readyState === 4 && xhr.status === 200) {
+    //         // const json = JSON.parse(xhr.responseText);
+    //         // console.log(json.nickname + ", " + json.password);
+    //     // }
+    // };
 
     xhr.send(s);
 

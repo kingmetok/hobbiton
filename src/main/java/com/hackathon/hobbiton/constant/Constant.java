88a7aaf4839f04611eacb83694fe6a7b5de82fbb
Mykhailo_Pinchuk <kingmetok@gmail.com>
2020-12-18 09:23:41 +0200

    Add a servlet for login and registration

--- src/main/java/com/hackathon/hobbiton/constant/Constant.java
+++
@@ -1,8 +0,0 @@
-package com.hackathon.hobbiton.constant;
-
-public class Constant {
-    private Constant() {}
-
-    public static final String MAN = "Male";
-    public static final String WOMAN = "Female";
-}
